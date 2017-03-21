12a1f076c6af17e71c2b03328400b660610c9c04
Thor Skou Hymøller <thorhymoeller@gmail.com>
2017-03-21 22:14:40 +0100

    Enemy + Waypoint system

---
+++ InteractionFirstProgramming/core/src/bachelor/project/nije214/thhym14/Enemy.java
@@ -0,0 +1,71 @@
+/**
+ * Created by Administrator on 20-03-2017.
+ */
+
+package bachelor.project.nije214.thhym14;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class Enemy extends Sprite {
+
+    private Vector2 velocity = new Vector2();
+    private float speed = 100;
+
+    private Array<Vector2> path;
+    private int waypoint = 0;
+
+    public Enemy(Sprite sprite, Array<Vector2> path) {
+        super(sprite);
+        this.path = path;
+    }
+
+    //@Override
+    public void draw(SpriteBatch spriteBatch) {
+        update(Gdx.graphics.getDeltaTime());
+        super.draw(spriteBatch);
+    }
+
+    public void update(float delta) {
+        //Vinklen mellem nuværende position og næste waypoint
+        //@@@BUG@@@ vinklen bliver udregnet fra spritens x,y(0,0) i stedet for midten af spriten hvilket giver en anelse forkert vinkel.. hmmm
+        float angle = (float) Math.atan2(path.get(waypoint).y - getY(), path.get(waypoint).x - getX());
+        //Hastighed på x og y aksen
+        velocity.set((float) Math.cos(angle) * speed, (float) Math.sin(angle) * speed);
+        //Position i forhold til tid
+        setPosition(getX() + velocity.x * delta, getY() + velocity.y * delta);
+        //setCenter(getX() + velocity.x * delta, getY() + velocity.y * delta);
+
+        setRotation(angle * MathUtils.radiansToDegrees);
+
+
+        if (isWaypointReached()) {
+            //setPosition(path.get(waypoint).x, path.get(waypoint).y);
+            setCenter(path.get(waypoint).x, path.get(waypoint).y);
+            if (waypoint + 1 >= path.size)
+                waypoint = 0;
+            else
+                waypoint++;
+        }
+    }
+
+    public boolean isWaypointReached() {
+        //Afstand til waypoint i forhold maks afstand enemy kan flytte sig på 1 frame
+        System.out.println((path.get(waypoint).x - getX()));
+        return Math.abs(path.get(waypoint).x - getX()) <= speed * Gdx.graphics.getDeltaTime() && Math.abs(path.get(waypoint).y - getY()) <= speed * Gdx.graphics.getDeltaTime();
+    }
+
+    public Array<Vector2> getPath() {
+        return path;
+    }
+
+    public int getWaypoint() {
+        return waypoint;
+    }
+
+}
+
---
+++ InteractionFirstProgramming/core/src/bachelor/project/nije214/thhym14/Waypoint.java
@@ -0,0 +1,129 @@
+package bachelor.project.nije214.thhym14;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class Waypoint implements Screen {
+
+    private ShapeRenderer sr;
+    private SpriteBatch batch;
+    private Array<Enemy> enemies;
+
+    private Sprite sprite;
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        for(Enemy enemy : enemies)
+            enemy.draw(batch);
+        batch.end();
+
+        //Tegn rute mellem waypoints som hvid linje
+        sr.setColor(Color.WHITE);
+        sr.begin(ShapeType.Line);
+        for(Enemy enemy : enemies) {
+            Vector2 previous = enemy.getPath().first();
+            for(Vector2 waypoint : enemy.getPath()) {
+                sr.line(previous, waypoint);
+                previous = waypoint;
+            }
+        }
+        sr.end();
+
+        //Tegn waypoints
+        sr.begin(ShapeType.Filled);
+        for(Enemy enemy : enemies)
+            for(Vector2 waypoint : enemy.getPath())
+                sr.circle(waypoint.x, waypoint.y, 10);
+        sr.end();
+
+
+        // /Tegn rute fra Enemy til nærmeste waypoint
+        sr.setColor(Color.CYAN);
+        sr.begin(ShapeType.Line);
+        for(Enemy enemy : enemies)
+            sr.line(new Vector2(enemy.getX() + enemy.getWidth()/2, enemy.getY() + enemy.getHeight()/2), enemy.getPath().get(enemy.getWaypoint()));
+        sr.end();
+
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+
+        sr = new ShapeRenderer();
+        batch = new SpriteBatch();
+
+        sprite = new Sprite(new Texture("badlogic.jpg"));
+        sprite.setSize(75, 75);
+        sprite.setOrigin(sprite.getHeight()/2, sprite.getWidth()/2);
+
+        enemies = new Array<Enemy>();
+        Enemy enemy = new Enemy(sprite, getPath());
+        //Enemy enemy2 = new Enemy(sprite, getPath2());
+
+
+        //enemy.setPosition(250, 0);
+        enemy.setCenter(250,0);
+        enemy.setOriginCenter();
+        //enemy2.setPosition(800, 1000);
+
+
+        enemies.add(enemy);
+        //enemies.add(enemy2);
+
+
+    }
+
+    private Array<Vector2> getPath() {
+        int height = Gdx.graphics.getHeight();
+        int width = Gdx.graphics.getWidth();
+        Array<Vector2> path = new Array<Vector2>();
+        path.add(new Vector2(250, 500));
+        path.add(new Vector2(800, 500));
+        path.add(new Vector2(800, 1000));
+        path.add(new Vector2(250, 1000));
+        path.add(new Vector2(250, 1500));
+        path.add(new Vector2(800, 1500));
+        path.add(new Vector2(800, height));
+        return path;
+    }
+
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        sr.dispose();
+        batch.dispose();
+        sprite.getTexture().dispose();
+    }
+
+}
