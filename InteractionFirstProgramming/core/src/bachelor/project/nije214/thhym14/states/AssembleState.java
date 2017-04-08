a0f803016f53b327e36e14361330c5dcf92ba073
Triah <nicolaihedegaardjensen93@gmail.com>
2017-04-08 02:43:32 +0200

    scrollpane and functional buttons

--- InteractionFirstProgramming/core/src/bachelor/project/nije214/thhym14/states/AssembleState.java
+++ InteractionFirstProgramming/core/src/bachelor/project/nije214/thhym14/states/AssembleState.java
@@ -7,15 +7,22 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+import java.util.LinkedList;
+
+import bachelor.project.nije214.thhym14.Enemy;
 
 import static bachelor.project.nije214.thhym14.StaticGlobalVariables.HEIGHT;
 import static bachelor.project.nije214.thhym14.StaticGlobalVariables.WIDTH;
@@ -31,26 +38,64 @@ public class AssembleState extends State {
     private TextButton button;
     private Table table;
     private TextButton button2;
+    private LinkedList<TextButton> textButtons;
+    private ScrollPane scrollPane;
+    private Enemy enemy;
 
     public AssembleState(GameStateManager gsm) {
         super(gsm);
         camera.setToOrtho(false, WIDTH, HEIGHT);
         camera.update();
+        createUIElements();
+        textButtons = new LinkedList<TextButton>();
+        for(int i = 0; i <= 10; i++){
+            setButtonAttributes("Speed", 400, 200, 2.5f);
+        }
+        enemy = new Enemy();
+        addButtonToTable();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    public void createUIElements(){
         skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
         stage = new Stage(new StretchViewport(WIDTH, HEIGHT));
         table = new Table();
+        scrollPane = new ScrollPane(table,skin);
+        scrollPane.setSize(800,HEIGHT/2);
+        scrollPane.setPosition(WIDTH/2-scrollPane.getWidth()/2,HEIGHT/2-scrollPane.getHeight()/2);
         table.setWidth(stage.getWidth());
-        table.align(Align.center|Align.top);
-        table.setPosition(0,HEIGHT);
-        button = new TextButton("Enemy", skin);
-        button2 = new TextButton("Map", skin);
-        button2.setWidth(500);
-        button2.setHeight(200);
-        button2.getLabel().setFontScale(2.5f);
-        table.add(button);
-        table.add(button2).width(button2.getWidth()).height(button2.getHeight());
-        stage.addActor(table);
-        Gdx.input.setInputProcessor(stage);
+        table.align(Align.left|Align.top);
+        stage.addActor(scrollPane);
+    }
+
+    public void chooseEnemySpeed(){
+        for(TextButton textButton : textButtons){
+            if(textButton.getLabel().getText().toString().matches("Speed")){
+                textButton.addListener(new ChangeListener() {
+                    @Override
+                    public void changed(ChangeEvent event, Actor actor) {
+                        enemy.setSpeed(150);
+                    }
+                });
+            }
+        }
+    }
+
+    public void setButtonAttributes(String buttonText, float width, float height, float fontScale){
+        TextButton textButton = new TextButton(buttonText,skin);
+        textButton.setWidth(width);
+        textButton.setHeight(height);
+        textButton.getLabel().setFontScale(fontScale);
+        textButtons.add(textButton);
+    }
+
+    public void addButtonToTable(){
+        for(int i = 0; i<textButtons.size();i++) {
+            table.add(textButtons.get(i)).width(textButtons.get(i).getWidth()).height(textButtons.get(i).getHeight());
+            if(i%2==0 && i!=0 && i!=textButtons.size()){
+                table.row();
+            }
+        }
     }
 
     @Override
@@ -61,12 +106,14 @@ public class AssembleState extends State {
     @Override
     public void update(float deltaTime) {
         stage.act();
+        chooseEnemySpeed();
     }
 
     @Override
     public void render(SpriteBatch sb) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
+        System.out.println(enemy.getSpeed());
     }
 
     @Override
